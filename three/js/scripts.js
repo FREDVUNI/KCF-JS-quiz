cb1a0e3276aac9e82e9dd22b4cb9f995a241e77c
TAATA EARL <fredvuni809@gmail.com>
2022-03-25 11:21:30 +0300

    Another try for question three

--- three/js/scripts.js
+++ three/js/scripts.js
@@ -1,14 +1,32 @@
-// let someArray = [3.0,'a',7,'x','20','d',4,'f',8]
 let arrayEven = [2,4,6,8,10]
 let arrayOdd = [1,3,5,7,9]
 let arrayChar =['q','w','e','r','t','y']
 
-let someArray ={
-    evens:[arrayEven],
-    odds:[arrayOdd],
-    chars:[arrayChar],
-}
+// let checkString = (theArray) =>{
+//     for ( let i=0; i< theArray.length; i++ ) {
+//         if (typeof theArray[i] != "string")
+//             return false;
+//     };
+//     return true;
+// }
+
+// let checkNumber = (theArray) =>{
+//     for ( let i=0; i< theArray.length; i++ ) {
+//         if (typeof theArray[i] != "number")
+//             return false;
+//     };
+//     return true;
+// }
+
+// if(checkNumber(arrayEven) || checkNumber(arrayOdd) || checkString(arrayChar)) console.log('Invalid array values.')
+
+let someArray =[
+    {evens:arrayEven,odds:arrayOdd,chars:arrayChar}
+]
 function someFunction(someArray){
-    console.log(someArray)
+    for(let i = 0; i < someArray.length; i++ ){
+        console.log(someArray[i])
+        return someArray[i]
+    }
 }
-someFunction({someArray})
+someFunction(someArray)
